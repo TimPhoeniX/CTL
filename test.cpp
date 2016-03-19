a00147073092710ee20658bd0df9995d5b91d12c
Piotr Grudzień <piotr.grudzien@outlook.com>
2016-03-19 22:45:25 +0100

    AiSD2 Assignment completed & tested on Windows. Todo: Check with Linux

--- test.cpp
+++ test.cpp
@@ -1,27 +1,55 @@
-#include "ctl_matrix.hpp"
-#include "ctl_stack.hpp"
-#include "ctl_proto_hashmap.hpp"
-#include <fstream>
-#include <iostream>
-#include <list>
+#include "ctl_virtual_disjointset.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-	CTL::Matrix<double> m(6,6);
-	auto data = m[0];
-	for(double i :
-	{	19./12.,	13./12.,	10./12.,	10./12.,	13./12.,	-17/12.,
-		13./12.,	13./12.,	10./12.,	10./12.,	-11./12.,	13./12.,
-		10./12.,	10./12.,	10./12.,	-2./12.,	10./12.,	10./12.,
-		10./12.,	10./12.,	-2./12.,	10./12.,	10./12.,	10./12.,
-		13./12.,	-11./12.,	10./12.,	10./12.,	13./12.,	13./12.,
-		-17./12.,	13./12.,	10./12.,	10./12.,	13./12.,	19./12.})
+	if(argc != 2)
 	{
-		*(data++)=i;
+		std::cout << "Illegal argc!\n";
+		return 0;
 	}
-	auto E1 = m.LeadingEigenvector();
-	std::cout << E1 << std::endl;
-	auto E2 = m.SubleadingEigenvector(E1);
-	std::cout << E2 << std::endl;
+	CTL::ForwardList<CTL::DNode<char>*> nodes;
+	int n = 0;
+	std::cin >> n;
+	for(int i = 0; i < n; ++i)
+	{
+		char c = ' ';
+		std::cin >> c;
+		if(argv[1][0]=='t')
+		{
+			nodes.PushBack(new CTL::InTree<char>(c));
+		}
+		else if(argv[1][0]=='l')
+		{
+			nodes.PushBack(new CTL::DListNode<char>(c));
+		}
+		else
+		{
+			std::cout << "Illegal argument\n";
+			return 0;
+		}
+	}
+	CTL::ForwardList<CTL::Edge<char>> edges;
+	std::cin >> n;
+	for(int i = 0; i < n; ++i)
+	{
+		char a = ' ';
+		char b = ' ';
+		std::cin >> a >> b;
+		CTL::Edge<char> e;
+		for(auto& v : nodes)
+		{
+			if(v->getVertex() == a) e.GetFirst() = v;
+			if(v->getVertex() == b) e.GetSecond() = v;
+		}
+		edges.PushBack(e);
+	}
+
+	auto comps = ConnectedComponents(edges, nodes);
+
+	for(auto s : comps)
+	{
+		s->PrintSet();
+	}
+
 	return 0;
 }
