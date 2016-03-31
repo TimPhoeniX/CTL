865239b1d774e000208e49f927e979685e28e868
Piotr Grudzień <piotr.grudzien@outlook.com>
2016-04-01 00:28:45 +0200

    ArrayList formating updated.

--- CTL/Container/ctl_arraylist.hpp
+++ CTL/Container/ctl_arraylist.hpp
@@ -1,4 +1,4 @@
-/**  
+/**
  * Conceptual Template Library by Piotr Grudzień
  * Simple ArrayList
  */
@@ -11,7 +11,7 @@
 namespace CTL
 {
 	template<typename T, typename Allocator = std::allocator<T> >
-	class ArrayList : private Allocator
+	class ArrayList: private Allocator
 	{
 	private:
 		using Alloc = std::allocator_traits<Allocator>;
@@ -27,67 +27,74 @@ namespace CTL
 		using const_pointer = typename Alloc::const_pointer;
 		using iterator = value_type*;
 		using const_iterator = const value_type*;
-		
+
 	private:
 		pointer storage = nullptr;
-		size_type cSize = 0 ;
+		size_type cSize = 0;
 		size_type cMaxSize = 1024;
-		
+
 		void MoveBack(size_type begin)
 		{
 			if(this->cSize > 0)
 			{
-				Alloc::construct(*this,this->storage+this->cSize,std::move(this->storage[cSize-1]));
-				for(size_type i = this->cSize-1; i > begin; --i)
+				Alloc::construct(*this, this->storage + this->cSize, std::move(this->storage[cSize - 1]));
+				for(size_type i = this->cSize - 1; i > begin; --i)
 				{
-					this->storage[i]=std::move(this->storage[i-1]);
+					this->storage[i] = std::move(this->storage[i - 1]);
 				}
 			}
 			++(this->cSize);
 		}
-		
+
 		void MoveFront(size_type begin)
 		{
-			for(size_type i = begin; i < cSize-1; ++i)
+			for(size_type i = begin; i < cSize - 1; ++i)
 			{
-				this->storage[i]=std::move(this->storage[i+1]);
+				this->storage[i] = std::move(this->storage[i + 1]);
 			}
-			Alloc::destroy(*this,this->storage+(--cSize));
+			Alloc::destroy(*this, this->storage + (--cSize));
 		}
-		
+
 	public:
 		ArrayList():
 			ArrayList(Allocator())
-		{
-		}
+		{}
 
 		explicit ArrayList(const Allocator& alloc):
 			Allocator(alloc)
-		{
-		}
+		{}
 
-		ArrayList(size_type count,const T& value,const Allocator& alloc = Allocator()):
+		ArrayList(size_type count, const T& value, const Allocator& alloc = Allocator()):
 			Allocator(alloc),
-			storage(Alloc::allocate(*this,count)),
+			storage(Alloc::allocate(*this, count)),
 			cSize(count),
 			cMaxSize(count)
 		{
-			//Todo
+			for(auto begin = this->storage, end = this->storage + count; begin != end; ++begin)
+			{
+				Alloc::construct(*this, begin, value);
+			}
 		}
-		
+
+		ArrayList(size_type count, const Allocator& alloc = Allocator()):
+			Allocator(alloc),
+			storage(Alloc::allocate(*this, count)),
+			cMaxSize(count)
+		{}
+
 		ArrayList(const ArrayList& other):
 			Allocator(Alloc::select_on_container_copy_construction(other.get_allocator())),
-			storage(Alloc::allocate(*this,other.cMaxSize)),
+			storage(Alloc::allocate(*this, other.cMaxSize)),
 			cSize(other.cSize),
 			cMaxSize(other.cMaxSize)
 		{
-			for(auto a = this->storage, b = other.storage, end = other.storage+other.cMaxSize; b != end; ++a,++b)
+			for(auto a = this->storage, b = other.storage, end = other.storage + other.cMaxSize; b != end; ++a, ++b)
 			{
 				Alloc::construct(*this, a, *b);
 			}
 		}
-		
-		ArrayList(ArrayList<value_type>&& l) :
+
+		ArrayList(ArrayList<value_type>&& l):
 			Allocator(std::move(l)),
 			storage(l.storage),
 			cSize(l.cSize),
@@ -95,17 +102,18 @@ namespace CTL
 		{
 			l.storage = nullptr;
 			l.cSize = 0;
+			l.cMaxSize = 0;
 		}
-		
+
 		~ArrayList()
 		{
-			for (auto ptr = this->storage,end = this->storage+this->cMaxSize; ptr != end; ++ptr )
+			for(auto ptr = this->storage, end = this->storage + this->cMaxSize; ptr != end; ++ptr)
 			{
-				Alloc::destroy(*this,ptr);
+				Alloc::destroy(*this, ptr);
 			}
-			Alloc::deallocate(*this,this->storage,this->cMaxSize);
+			Alloc::deallocate(*this, this->storage, this->cMaxSize);
 		}
-		
+
 		allocator_type get_allocator() const
 		{
 			return *this;
@@ -115,90 +123,90 @@ namespace CTL
 		{
 			return this->storage;
 		}
-		
+
 		iterator end()
 		{
-			return this->storage+this->cSize;
+			return this->storage + this->cSize;
 		}
-		
+
 		const_iterator begin() const
 		{
 			return this->storage;
 		}
-		
+
 		const_iterator end() const
 		{
-			return this->storage+this->cSize;
+			return this->storage + this->cSize;
 		}
-		
+
 		const_iterator cbegin()
 		{
 			return this->storage;
 		}
-		
+
 		const_iterator cend()
 		{
-			return this->storage+this->cSize;
+			return this->storage + this->cSize;
 		}
-		
+
 		bool empty() const
 		{
 			return this->cSize == 0;
 		}
-		
+
 		size_type size() const
 		{
 			return this->cSize;
 		}
-		
+
 		size_type max_size() const
 		{
 			return this->cMaxSize;
 		}
-		
-		void PushFront(const value_type& e)
+
+		void push_front(const value_type& e)
 		{
 			if(this->cSize < this->cMaxSize)
 			{
 				this->MoveBack(0);
-				*(this->storage)=e;
+				*(this->storage) = e;
 			}
 		}
-		
-		void PushFront(value_type&& e)
+
+		void push_front(value_type&& e)
 		{
 			if(this->cSize < this->cMaxSize)
 			{
 				this->MoveBack(0);
-				(*this->storage)=std::move(e);
+				(*this->storage) = std::move(e);
 			}
 		}
-		
+
 		void push_back(const value_type& e)
 		{
 			if(this->cSize < this->cMaxSize)
 			{
-				Alloc::construct(*this,this->storage+(cSize++),e);
+				Alloc::construct(*this, this->storage + (cSize++), e);
 			}
 		}
-		
+
 		void push_back(value_type&& e)
 		{
 			if(this->cSize < this->cMaxSize)
 			{
-				std::allocator_traits<allocator_type>::construct(*this,this->storage+(cSize++),std::move(e));
+				std::allocator_traits<allocator_type>::construct(*this, this->storage + (cSize++), std::move(e));
 			}
 		}
-		
+
 		//Standarize later;
 		value_type pop_back()
 		{
 			if(this->cSize == 0) throw std::out_of_range("PopBack called on empty list");
 			value_type tmp = std::move(this->storage[--this->cSize]);
-			std::allocator_traits<allocator_type>::destroy(*this,this->storage+this->cSize);
+			std::allocator_traits<allocator_type>::destroy(*this, this->storage + this->cSize);
 			return tmp;
 		}
-		
+
 		value_type pop_front()
 		{
 			if(this->cSize == 0) throw std::out_of_range("PopFront called on empty list");
@@ -206,70 +214,70 @@ namespace CTL
 			this->MoveFront(0);
 			return tmp;
 		}
-		
+
 		reference get(const size_type i)
 		{
 			if(this->cSize <= i) throw std::out_of_range("Get called with i >= cSize");
 			return this->storage[i];
 		}
-		
+
 		const_reference get(const size_type i) const
 		{
 			if(this->cSize <= i) throw std::out_of_range("Get called with i >= cSize");
 			return this->storage[i];
 		}
-		
+
 		void insert(const size_type i, const value_type& e)
 		{
 			if(this->cSize < this->cMaxSize)
 			{
 				this->MoveBack(i);
-				this->storage[i]=e;
+				this->storage[i] = e;
 			}
 		}
-		
+
 		void insert(const size_type i, value_type&& e)
 		{
 			if(this->cSize < this->cMaxSize)
 			{
 				this->MoveBack(i);
-				this->storage[i]=std::move(e);
+				this->storage[i] = std::move(e);
 			}
 		}
-		
+
 		void insert(const_iterator i, const value_type& e)
 		{
-			this->insert(i-this->storage,e);
+			this->insert(i - this->storage, e);
 		}
-		
+
 		void insert(const_iterator i, value_type&& e)
 		{
-			this->insert(i-this->storage,std::move(e));
+			this->insert(i - this->storage, std::move(e));
 		}
-		
+
 		iterator find(const value_type& e)
 		{
 			for(size_type i = 0; i < this->cSize; ++i)
 			{
-				if(this->storage[i] == e) return this->storage+i;
+				if(this->storage[i] == e) return this->storage + i;
 			}
 			return this->end();
 		}
-		
+
 		void Erase(const size_type i)
 		{
 			if(i < this->cSize) this->MoveFront(i);
 		}
-		
+
 		void clear()
 		{
 			for(auto ptr = this->storage, end = this->storage + this->cMaxSize; ptr != end; ++ptr)
 			{
 				Alloc::destroy(*this, ptr);
 			}
-			this->cSize=0;
+			this->cSize = 0;
 		}
-		
+
 		void Print(std::ostream& out = std::cout) const
 		{
 			for(size_type i = 0; i < this->cSize; ++i)
@@ -277,17 +285,17 @@ namespace CTL
 				out << this->storage[i] << ' ';
 			}
 		}
-		
+
 		reference operator[](const size_type i)
 		{
 			return this->storage[i];
 		}
-		
+
 		const_reference operator[](const size_type i) const
 		{
 			return this->storage[i];
 		}
-		
+
 		friend std::ostream& operator<<(std::ostream& out, const CTL::ArrayList<value_type>& l)
 		{
 			l.Print(out);
