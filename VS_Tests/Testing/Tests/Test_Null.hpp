710e3b77444da3c93fe5356542c0351bad049e09
Piotr Grudzień <piotr.grudzien@outlook.com>
2016-04-30 22:32:15 +0200

    Properly calling pointer to member function.

--- VS_Tests/Testing/Tests/Test_Null.hpp
+++ VS_Tests/Testing/Tests/Test_Null.hpp
@@ -4,7 +4,7 @@
 class nulltest
 {
 public:
-	bool isNull()
+	bool isNull() const
 	{
 		return this == nullptr;
 	}
@@ -13,7 +13,10 @@ public:
 void test_null()
 {
 	nulltest* ptr = nullptr;
+	bool (nulltest::*f)() const = &nulltest::isNull;
 
+	std::cout << "ptr is " << ( (ptr->*f) () ? "nullptr" : "nulltest*") << std::endl;
+	std::cout << "ptr is " << ( (*ptr.*f) () ? "nullptr" : "nulltest*") << std::endl;
 	std::cout << "ptr is " << (ptr->isNull() ? "nullptr" : "nulltest*") << std::endl;
 }
 
